1c203e52eb1d4645bc1ed664703d002b88a95e16
steph-d989 <steb_d@hotmail.com>
2025-08-24 21:51:07 -0500

    corrigindo error en model

--- models/product.py
+++ models/product.py
@@ -1,9 +1,23 @@
-from dataclasses import dataclass
+""" from dataclasses import dataclass
 from typing import List
 from datetime import datetime
 
 @dataclass
 class Product:
+    nombre: str
+    precio: float
+    categorias: List[str]
+    en_venta: bool
+    ts: datetime
+ """
+
+from dataclasses import dataclass
+from typing import List, Optional
+from datetime import datetime
+
+@dataclass
+class Product:
+    id: Optional[int]
     nombre: str
     precio: float
     categorias: List[str]
